f7dd59b696b128a38baa2ac538ef176d0eb8c268
joshuahuggins-prog <joshua.huggins@googlemail.com>
2026-01-30 23:09:39 +0000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,46 @@ import { initStorage } from "./utils/storage";
 
 initStorage();
 
+// TEMP DEBUG: capture InvalidCharacterError source on white-screen crashes
+(() => {
+  const wrap = (obj, name) => {
+    const orig = obj && obj[name];
+    if (!orig) return;
+    obj[name] = function (...args) {
+      try {
+        return orig.apply(this, args);
+      } catch (e) {
+        // Log EVERYTHING we can while still allowing the crash to show up
+        console.error(`🔥 THROW in ${name}`, { args, error: e, thisObj: this });
+        throw e;
+      }
+    };
+  };
+
+  // Most common sources of InvalidCharacterError
+  wrap(Document.prototype, "createElement");
+  wrap(Document.prototype, "createElementNS");
+
+  wrap(DOMTokenList.prototype, "add");
+  wrap(DOMTokenList.prototype, "remove");
+  wrap(DOMTokenList.prototype, "toggle");
+  wrap(DOMTokenList.prototype, "replace");
+
+  wrap(Document.prototype, "querySelector");
+  wrap(Document.prototype, "querySelectorAll");
+  wrap(Element.prototype, "querySelector");
+  wrap(Element.prototype, "querySelectorAll");
+  wrap(Element.prototype, "matches");
+
+  // Also grab global errors
+  window.addEventListener("error", (evt) => {
+    console.error("🔥 window.error", evt?.error || evt);
+  });
+  window.addEventListener("unhandledrejection", (evt) => {
+    console.error("🔥 unhandledrejection", evt?.reason || evt);
+  });
+})();
+
 // PWA service worker
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
